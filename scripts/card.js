7ebc0547b01dbfadd67b858d80dfad82bd71cc3b
António Pinto <antoniopintotrabalho@gmail.com>
2022-05-11 10:07:55 +0100

    Initial commit

---
+++ scripts/card.js
@@ -0,0 +1,53 @@
+let constrain = 150;
+let mouseOverContainer = document.getElementById("ex1");
+let ex1Layer = document.getElementById("ex1-layer");
+let ex1Layer2 = document.getElementById("ex1-layer2");
+let layer = ex1Layer; 
+let cardFlipped = false
+
+function transforms(x, y, el) {
+    let box = el.getBoundingClientRect();
+    let calcX = -(y - box.y - (box.height / 2)) / constrain;
+    let calcY = (x - box.x - (box.width / 2)) / constrain * 2;
+
+    if(cardFlipped) calcY = 180 - calcY;
+    return "perspective(100px) "
+        + "   rotateX(" + calcX + "deg) "
+        + "   rotateY(" + calcY + "deg) ";
+};
+
+function transformElement(el, xyEl) {
+    el.style.transform = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function (e) {
+    let xy = [e.clientX, e.clientY];
+    let position = xy.concat([layer]);
+
+    window.requestAnimationFrame(function () {
+        transformElement(layer, position);
+    });
+};
+
+mouseOverContainer.onclick = (e) => {
+    let frontCard = document.getElementsByClassName("flip-card-inner")[0]
+   
+    cardFlipped ? frontCard.style.transform = "" : frontCard.style.transform = "rotateY(180deg)";
+
+    cardFlipped = !cardFlipped
+    
+    layer = !cardFlipped ? ex1Layer : ex1Layer2;
+}
+
+mouseOverContainer.onmouseleave = (e) =>{
+    const box = layer.getBoundingClientRect()
+    const xCenter = (box.left + box.right) / 2
+    const yCenter = (box.top + box.bottom) / 2
+    let xy = [xCenter, yCenter];
+    let position = xy.concat([layer]);
+
+    window.requestAnimationFrame(function () {
+        transformElement(layer, position);
+    });
+}
+
